ac86a0c213bb3c28cced058fcf6584347fdc4672
Kous <kous.takken@gmail.com>
2022-08-08 11:58:29 +0500

    some more

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { Routes, Route } from 'react-router-dom';
 //components
 import Header from './components/Header';
+//pages
+import Home from './pages/home';
+import Page404 from './pages/Page404';
 //scss
 import './scss/app.scss';
 
@@ -7,6 +11,12 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <div className="wrapper">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </div>
     </div>
   );
 }
